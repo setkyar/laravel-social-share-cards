bca07fc3798796def3ce3fd8a04d08d294138594
Enaah <mathis.bin@icloud.com>
2018-02-25 19:57:39 +0100

    update/Tabs to Spaces
    - Converts tabs to spaces to use consistent spacing-rule

--- src/views/meta-article.blade.php
+++ src/views/meta-article.blade.php
@@ -1,32 +1,32 @@
 @if(isset($title,$description,$image) && !empty($title) && !empty($description) && !empty($image))
-	<!-- All Common -->
-	<meta property="og:type"            content="article" />
-	<meta property="og:image"           content="{{ $image }}" />
+    <!-- All Common -->
+    <meta property="og:type"            content="article" />
+    <meta property="og:image"           content="{{ $image }}" />
 
-	@if(isset($author) && $author !=='')
-	<meta name="author" 				content="{{ $author }}" />
-	<meta name="twitter:creator" 		content="{{ $author }}">
-	@endif
+    @if(isset($author) && $author !=='')
+    <meta name="author"                 content="{{ $author }}" />
+    <meta name="twitter:creator"        content="{{ $author }}">
+    @endif
 
-	<!-- Facebook -->
-	<meta property="og:url"             content="{{ url()->current() }}" />
-	<meta property="og:title"           content="{{ $title }}" />
-	<meta property="og:description"     content="{{ $description }}" />
-	@if(config('social.fb_app_id') !=='')
-		<meta name="fb:app_id" 			content="{{  config('social.fb_app_id') }}">
-	@endif
+    <!-- Facebook -->
+    <meta property="og:url"             content="{{ url()->current() }}" />
+    <meta property="og:title"           content="{{ $title }}" />
+    <meta property="og:description"     content="{{ $description }}" />
+    @if(config('social.fb_app_id') !=='')
+        <meta name="fb:app_id"          content="{{  config('social.fb_app_id') }}">
+    @endif
 
 
-	<!-- Twitter Summary Card With Large Image -->
-	<meta name="twitter:card" 			content="summary_large_image">
-	@if(config('social.twitter_site') !=='')
-		<meta name="twitter:site" 			content="{{  config('social.twitter_site') }}">
-	@endif
-	<meta name="twitter:title" 			content="{{ $title }}">
-	<meta name="twitter:description" 	content="{{ $description }}">
-	<meta name="twitter:image" 			content="{{ $image }}">
+    <!-- Twitter Summary Card With Large Image -->
+    <meta name="twitter:card"           content="summary_large_image">
+    @if(config('social.twitter_site') !=='')
+        <meta name="twitter:site"       content="{{  config('social.twitter_site') }}">
+    @endif
+    <meta name="twitter:title"          content="{{ $title }}">
+    <meta name="twitter:description"    content="{{ $description }}">
+    <meta name="twitter:image"          content="{{ $image }}">
 
-	<!-- Google Plus Full-Bleed Image -->
-	<meta itemprop="og:headline" 		content="{{ $image }}" />
-	<meta itemprop="og:description" 	content="{{ $description }}" />
+    <!-- Google Plus Full-Bleed Image -->
+    <meta itemprop="og:headline"        content="{{ $image }}" />
+    <meta itemprop="og:description"     content="{{ $description }}" />
 @endif
